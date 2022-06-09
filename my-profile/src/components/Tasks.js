efbd1ef17ed2a0fbba5b5351d8ff34d367b7b386
Rico The Dev <ricothedev@gmail.com>
2022-06-09 09:02:42 -0700

    Update Tasks.js

--- my-profile/src/components/Tasks.js
+++ my-profile/src/components/Tasks.js
@@ -1,28 +1,30 @@
-const tasks = [
-  {
-    id: 1,
-    text: "Doctors Appointment",
-    day: "Feb 5th at 2:30pm",
-    reminder: true
-  },
-  {
-    id: 2,
-    text: "Meeting at School",
-    day: "Feb 6th at 1:30pm",
-    reminder: true
-  },
-  {
-    id: 3,
-    text: "Grocery Store",
-    day: "Feb 5th at 2:30pm",
-    reminder: false
-  }
-]
+import {useState} from 'react'
 
 const Tasks = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true
+    },
+    {
+      id: 2,
+      text: "Meeting at School",
+      day: "Feb 6th at 1:30pm",
+      reminder: true
+    },
+    {
+      id: 3,
+      text: "Grocery Store",
+      day: "Feb 5th at 2:30pm",
+      reminder: false
+    }
+  ])
+
   return (
     <>
-      {tasks.map((tasks) => (<h3>{tasks.text}</h3>))}
+      {tasks.map((tasks) => (<h3 key={tasks.id}>{tasks.text}</h3>))}
     </>
   )
 }
